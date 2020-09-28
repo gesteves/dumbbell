d3a13bd8d3e1901422d9299f7a7a7521866a01ee
Guillermo Esteves <g@gesteves.com>
2020-09-28 16:37:48 -0600

    Support for more than 10 products

--- amazon.rb
+++ amazon.rb
@@ -9,31 +9,38 @@ class Amazon
 
   def check_inventory
     products = YAML.load_file('products.yml')['products']
-    item_ids = products.map { |p| p['asin'] }.take(10)
-    resources = [
-     'Images.Primary.Large',
-     'ItemInfo.Title',
-     'Offers.Listings.Availability.Type',
-     'Offers.Listings.DeliveryInfo.IsAmazonFulfilled',
-     'Offers.Listings.DeliveryInfo.IsPrimeEligible',
-     'Offers.Listings.Price'
-    ]
-    response = @client.get_items(item_ids: item_ids, resources: resources, condition: 'New')
-    if response.status == 200
-      items = response.to_h.dig('ItemsResult', 'Items')
-      items.each do |item|
-        amazon_listing = item.dig('Offers', 'Listings')&.find { |l| l.dig('Availability', 'Type') == 'Now' && l.dig('DeliveryInfo', 'IsAmazonFulfilled') && l.dig('DeliveryInfo', 'IsPrimeEligible') }
-        unless amazon_listing.nil?
-          title = item.dig('ItemInfo', 'Title', 'DisplayValue')
-          url = item.dig('DetailPageURL')
-          image = item.dig('Images', 'Primary', 'Large', 'URL')
-          price = amazon_listing.dig('Price', 'DisplayAmount')
-          notify_slack(title: title, url: url, image: image, price: price)
-        end
-      end
-    else
-      puts "[ERROR] #{response.status} – #{response.body}"
+    item_ids = products.map { |p| p['asin'] }.each_slice(10).to_a
+    item_ids.each do |ids|
+      get_items(item_ids: ids)
+      sleep 1
     end
+  end
+
+  def get_items(item_ids:)
+    resources = [
+      'Images.Primary.Large',
+      'ItemInfo.Title',
+      'Offers.Listings.Availability.Type',
+      'Offers.Listings.DeliveryInfo.IsAmazonFulfilled',
+      'Offers.Listings.DeliveryInfo.IsPrimeEligible',
+      'Offers.Listings.Price'
+     ]
+     response = @client.get_items(item_ids: item_ids, resources: resources, condition: 'New')
+     if response.status == 200
+       items = response.to_h.dig('ItemsResult', 'Items')
+       items.each do |item|
+         amazon_listing = item.dig('Offers', 'Listings')&.find { |l| l.dig('Availability', 'Type') == 'Now' && l.dig('DeliveryInfo', 'IsAmazonFulfilled') && l.dig('DeliveryInfo', 'IsPrimeEligible') }
+         unless amazon_listing.nil?
+           title = item.dig('ItemInfo', 'Title', 'DisplayValue')
+           url = item.dig('DetailPageURL')
+           image = item.dig('Images', 'Primary', 'Large', 'URL')
+           price = amazon_listing.dig('Price', 'DisplayAmount')
+           notify_slack(title: title, url: url, image: image, price: price)
+         end
+       end
+     else
+       puts "[ERROR] #{response.status} – #{response.body}"
+     end
   end
 
   def notify_slack(title:, url:, image:, price:)
